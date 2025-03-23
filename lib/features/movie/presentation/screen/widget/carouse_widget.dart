09134702b20407b77821e6d301f4619403d5b703
HuanDo22222 <huando22222@gmail.com>
2025-03-24 02:45:13 +0700

    fix them

--- lib/features/movie/presentation/screen/widget/carouse_widget.dart
+++ lib/features/movie/presentation/screen/widget/carouse_widget.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:vie_flix/common/styles/app_color.dart';
 import 'package:vie_flix/config/routes/app_route.dart';
 import 'package:vie_flix/features/movie/domain/entity/card_entity.dart';
 import 'package:vie_flix/features/user/presentation/controller/app_setting_controller.dart';
@@ -69,12 +70,17 @@ class CarouseWidget extends StatelessWidget {
                               children: [
                                 Text(
                                   data[index].name,
-                                  style:
-                                      Theme.of(context).textTheme.titleMedium,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium!
+                                      .copyWith(color: AppColor.darkText),
                                 ),
                                 Text(
                                   data[index].originName,
-                                  style: Theme.of(context).textTheme.titleSmall,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .copyWith(color: AppColor.darkText),
                                 ),
                               ],
                             );
